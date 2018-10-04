a85d9738c566dd9c1779c05be8d77133160495c3
bebeto9009 <adr.pawlak@gmail.com>
2018-10-05 00:41:49 +0200

    second commit

--- src/js/app.js
+++ src/js/app.js
@@ -1,21 +1,35 @@
-const playerList = 'https://raw.githubusercontent.com/Bebeto9009/the_game/master/player.json';
-let playerArr = [];
+const firstPlayer = 'https://raw.githubusercontent.com/Bebeto9009/the_game/master/player.json';
 let obj = [];
+let playerArray = [];
 
-fetch(playerList)
-    .then(function (res) {
-        if (res.ok){
-            return res.json();
+fetch(firstPlayer)
+    .then(function (response) {
+        if(response.ok) {
+            return response.json();
         } else {
-            throw new Error ("Błąd połączenia");
+            throw new Error ('Błąd połączenia');
         }
-    })
+})
     .then(function (data) {
         obj = data;
-        playerArr = obj.map(el => Object.values(el));
-        console.log(playerArr[0]);
-    })
-    .catch(function(error) {
-        console.log("Błąd: ", error);
-    });
+        playerArray = obj.map(el => Object.values(el));
+        let noPlayer = document.querySelector('.game-ordinar-numbers');
+        let namePlayer = document.querySelector('.game-name');
 
+        noPlayer.value = playerArray[0][0];
+        namePlayer.value = playerArray[0][1];
+    })
+    .catch(function (error) {
+        console.log("Błąd: ", error);
+    }); //fetch json first player
+
+let list = document.querySelector('.game__section');
+
+function newPlayer(){
+    let noNewPlayer = document.createElement('input');
+    noNewPlayer.classList.add('game-ordinar-numbers');
+    // noNewPlayer.innerHTML =
+}
+
+let btnNewPlayer = document.querySelector('.btn-add');
+btnNewPlayer.addEventListener('click', newPlayer);
